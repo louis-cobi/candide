f45add9578c59aaf03dbfa872ad3d7d015d942d0
Manon Lavenier <manon.lavenier@gmail.com>
2023-03-16 18:37:30 +0100

    Add trees and infos

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 import React, { useState, useRef, useEffect } from "react";
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useLoader } from "@react-three/fiber";
 import { DoubleSide } from "three";
 import Formes from "./Formes.png"
 import Formes2 from "./Formes2.png"
 import { Html, Sky, Clone, useGLTF, OrbitControls, PerspectiveCamera } from '@react-three/drei';
 import { Vector3, BoxBufferGeometry, CylinderGeometry, Mesh, MeshBasicMaterial, TextureLoader } from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import huaranhuay from './models/huaranhuay.glb'
+import pisonay from './models/pisonay.glb'
 
-// Drei is a really helpful library
-// It has helpers for react-three-fiber
 
 
 import "./style.css";
 
-// Our main React component renders a Canvas from
-// react-three-fiber. The Canvas component does most
-// of the hard work of setting up the scene, renderer
-// and other core components of Three.js
 export default function App() {
 
   const [shape, setShape] = useState('cylinder')
@@ -36,7 +33,7 @@ export default function App() {
       "./Grass004_1K_Color.jpg",
       (loadedTexture) => {
         setTexture(loadedTexture);
-        console.log(texture)
+        // console.log(texture)
       },
 
       undefined,
@@ -44,18 +41,20 @@ export default function App() {
         console.error(error);
       }
     );
+
   }, []);
+
+  const [isShown, setIsShown] = useState(false);
+
+  const handleClick = event => {
+    // toggle shown state
+    setIsShown(current => !current);
+  };
 
   return (
     <div>
       <div style={{ width: '60%', height: '100%', float: 'left' }}>
         <Canvas style={{ height: "100vh", width: "100vw" }}>
-          {/*
-          A group is used for grouping, kind og like
-          groups in SVGs. The positioning of elements
-          inside a group is relative to the group's
-          position.
-        */}
           <Sky sunPosition={new Vector3(100, 10, 100)} />
           <group>
             {/* All these are in the same group */}
@@ -73,25 +72,13 @@ export default function App() {
                 )}
               <meshBasicMaterial map={texture} />
             </mesh>
-            {/* <ToolTip1 />
-          <ToolTip2 /> */}
-            {/* <ToolTip3 /> */}
           </group>
-          {/* Let there be light! */}
           <ambientLight />
-          {/*
-          Use a PerspectiveCamera.
-          PerspectiveCameras work like real works cameras
-          and provide depth perception.
-        */}
-          {/*
-          This lets you rotate the camera.
-          We've associated our React ref with it
-          like we would do for any React component
-        */}
-
+          <MyModel />
+          <MySecondModel />
         </Canvas>
       </div>
+
       <div style={{ width: '40%', height: '80%', float: 'left' }}>
         <div className="menu">
           <div className="menu-title">Formes</div>
@@ -131,6 +118,11 @@ export default function App() {
             <button>Mi-ombre</button>
             <button>Soleil partiel</button>
           </div>
+          <div className="menu-title">Information</div>
+          <div className="dimensions">
+            <button onClick={handleClick}>Click</button>
+            {isShown && <Box />}
+          </div>
         </div>
       </div>
     </div>
@@ -138,88 +130,47 @@ export default function App() {
   );
 }
 
-// This is the thing we are interested in
-// The GreenSquare component renders a mesh.
-// Meshes are objects that can have a shape and
-// texture.
-function GreenSquare() {
+// export function Tree({ position, rotation }) {
+//   const tree = useGLTF('https://douges.dev/static/tree.glb');
+//   console.log(tree);
+
+//   return (
+//     <group name="tree" rotation={rotation} position={[0, 0, 0]}>
+//       <Clone
+//         receiveShadow
+//         castShadow
+//         object={tree.nodes.trunk}
+//         inject={<meshBasicMaterial/>}
+//       />
+//       <Clone 
+//         receiveShadow
+//         castShadow 
+//         object={tree.nodes.foliage} 
+//         inject={<meshBasicMaterial/>} />
+//     </group>
+//   );
+// }
+
+export function MyModel() {
+  const { scene } = useGLTF(huaranhuay)
   return (
-    // The mesh is at the origin
-    // Since it is inside a group, it is at the origin
-    // of that group
-    // It's rotated by 90 degrees along the X-axis
-    // This is because, by default, planes are rendered
-    // in the X-Y plane, where Y is the up direction
-    <mesh position={[-4, 0, 0]} rotation={[Math.PI / 2, 0, 0]} scale={[1, 1, 1]}>
-      {/*
-        The thing that gives the mesh its shape
-        In this case the shape is a flat plane
-      */}
-      <boxGeometry args={[5, 5, 0.2]} />
-      {/*
-        The material gives a mesh its texture or look.
-        In this case, it is just a uniform green
-      */}
-      <meshBasicMaterial color="green" side={DoubleSide} />
-    </mesh>
+    <primitive object={scene} position={[0, 0, 0]} /> 
+    
+  )
+}
+
+export function MySecondModel() {
+  const { scene } = useGLTF(pisonay)
+  return (
+    <primitive object={scene} position={[1, 0, 0]} /> 
+  )
+}
+
+function Box() {
+  return (
+    <div className="test">
+      <p>Les plantes sont des organismes photosynthétiques et autotrophes, caractérisés par des cellules végétales. Elles forment l'un des règnes des Eukaryota. Ce règne est un groupe monophylétique comprenant les plantes terrestres</p>
+    </div>
   );
 }
 
-function Plant() {
-  return (
-    <mesh position={[-10, 0.35, 0]} receiveShadow castShadow>
-      <boxGeometry args={[0.15, 0.5, 0.2]} />
-      <meshLambertMaterial color={"darkGreen"} />
-    </mesh>
-  );
-};
-// The rest of the components are just tooltips
-// Drei's Html component lets you render any HTML
-// inside the 3d scene. It follows the same rules
-// as everything else when it comes to positioning,
-// but is not actually rendered inside the canvas
-
-// function ToolTip1() {
-//   return (
-//     <Html center position={[-1, 1, -1]}>
-//       <div>
-//         <p>Formes</p>
-//         <p>Surface disponible</p>
-//         <p>Type de sol</p>
-//         <p>Exposition</p>
-//       </div>
-//     </Html>
-//   );
-// }
-
-// function ToolTip2() {
-//   return (
-//     <Html center position={[1, -1, -1]}>
-//       <p>Scroll to zoom in and out</p>
-//     </Html>
-//   );
-// }
-
-// function ToolTip3() {
-//   return (
-//     <Html center position={[-1, 1, 1]}>
-//       <p>{"<== Code's on the left, with details in the comments"}</p>
-//     </Html>
-//   );
-// }
-
-export function Tree({ position, rotation }) {
-  const tree = useGLTF('https://douges.dev/static/tree.glb');
-
-  return (
-    <group name="tree" rotation={rotation} position={[-4, 0, 0]}>
-      <Clone
-        receiveShadow
-        castShadow
-        object={tree.nodes.trunk}
-        inject={<meshBasicMaterial color="black" />}
-      />
-      <Clone receiveShadow castShadow object={tree.nodes.foliage} />
-    </group>
-  );
-}
